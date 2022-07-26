3df79ebc6dd5d0e21e563fc28b2060487eb64674
Irfan Abidin <irfan43abdin@gmail.com>
2022-07-26 16:46:46 +0700

    not active redis2

--- src/routers/products.js
+++ src/routers/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productsController = require("../controller/products");
 const { protect, isAdmin } = require("../middlewares/auth");
 const upload = require("../middlewares/upload");
-const { hitCacheProductDetail, clearCacheProductDetail } = require("../middlewares/redis");
+// const { hitCacheProductDetail, clearCacheProductDetail } = require("../middlewares/redis");
 
 router
   .get("/", productsController.getProducts)
