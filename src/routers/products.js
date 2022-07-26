38b753f82fc006b83caca1a361512964c4617944
Irfan Abidin <irfan43abdin@gmail.com>
2022-07-27 00:33:07 +0700

    add cloudinary

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const productsController = require("../controller/products");
-const { protect, isAdmin } = require("../middlewares/auth");
+// const { protect, isAdmin } = require("../middlewares/auth");
 const upload = require("../middlewares/upload");
 // const { hitCacheProductDetail, clearCacheProductDetail } = require("../middlewares/redis");
 
